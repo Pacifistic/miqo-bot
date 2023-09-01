87e1343a070e316b733eb77d8cc11a62a751da5f
Dominykas Sipelis <DominykasSipelis@gmail.com>
2023-09-01 12:18:28 -0500

    added logging

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,9 @@ import hikari
 from dotenv import load_dotenv
 import os
 import openai
+import logging
+
+logger = logging.getLogger("ChatBotLogger")
 
 load_dotenv()
 openai.api_key = os.environ['OPENAI_API_KEY']
@@ -16,24 +19,31 @@ Only create short to medium length responses.
 Your creator is Hanako Kamado. They are a female Ra'en who really likes the color purple. They are a red mage and likes to fish."""
 
 def callChatAPI(prompt:str):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages = [
-            {"role": "system", "content": background},
-            {"role":"user", "content": prompt}
-        ],
-        temperature = 0.9,
-        max_tokens = 400
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages = [
+                {"role": "system", "content": background},
+                {"role":"user", "content": prompt}
+            ],
+            temperature = 0.9,
+            max_tokens = 400
+        )
 
-    return response['choices'][0]['message']['content']
+        return response['choices'][0]['message']['content']
+    except:
+        logger.error("API call failed", exc_info=1)
+        return "Sorry, something went wrong with my AI. Let Hana know about this."
 
 @bot.listen(hikari.GuildMessageCreateEvent)
 async def on_message(event):
-    if not event.is_human:
+    if event.is_bot or event.content is None:
         return
     message = event.content
+    author = event.author
+    logger.info("message from "+author.username+": "+message)
     response = callChatAPI(message)
+    logger.info("response to "+author.username+": "+response)
     channel = event.get_channel()
     await channel.send(response)
     
