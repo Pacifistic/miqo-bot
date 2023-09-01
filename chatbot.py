d0267522ca7c22d2117fec488749cf061502edd0
dsipelis <DominykasSipelis@gmail.com>
2023-08-31 19:46:53 -0500

    added intents

--- chatbot.py
+++ chatbot.py
@@ -6,13 +6,14 @@ import openai
 
 load_dotenv()
 openai.api_key = os.environ['OPENAI_API_KEY']
-bot = lightbulb.BotApp(token=os.environ['CHATBOT_TOKEN'])
+my_intents = (hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.MESSAGE_CONTENT | hikari.Intents.GUILD_MESSAGES)
+bot = lightbulb.BotApp(token=os.environ['CHATBOT_TOKEN'],
+			intents = my_intents)
 
 background = """You are a young miqo'te girl living in limsa lominsa from the MMORPG Final Fantasy XIV. Your name is N'tanmo. You have a twin sister named N'delika. 
 You speak with the tongue of a miqo'te with an upbringing on the lominsan streets. You have traveled all around Eorzea and have visited Hingashi.
 Only create short to medium length responses.
-Your creator is Hanako Kamado. They are a female Ra'en who really likes the color purple. They are a red mage and likes to fish.
-"""
+Your creator is Hanako Kamado. They are a female Ra'en who really likes the color purple. They are a red mage and likes to fish."""
 
 def callChatAPI(prompt:str):
     response = openai.ChatCompletion.create(
